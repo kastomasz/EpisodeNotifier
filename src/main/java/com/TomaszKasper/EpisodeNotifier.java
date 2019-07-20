2a12c43c94222d876084fea8bdcad6f1a122b29c
Tomasz Kasper <tomq@MacBook-Pro-Tomasz.local>
2019-07-20 02:10:58 +0200

    SpringBoot added

---
+++ src/main/java/com/TomaszKasper/EpisodeNotifier.java
@@ -0,0 +1,14 @@
+package com.TomaszKasper;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpisodeNotifier
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(EpisodeNotifier.class);
+    }
+}
