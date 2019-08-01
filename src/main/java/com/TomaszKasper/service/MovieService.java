db8d832e55974e77746d6adcc006f672e6bbc550
Tomasz Kasper <tomasz.kasperowicz@icloud.com>
2019-08-02 00:38:12 +0200

    RestController for Movie

--- src/main/java/com/TomaszKasper/service/MovieService.java
+++ src/main/java/com/TomaszKasper/service/MovieService.java
@@ -2,18 +2,23 @@ package com.TomaszKasper.service;
 
 import com.TomaszKasper.model.Movie;
 import com.TomaszKasper.repository.MovieRepository;
+import javassist.NotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class MovieService {
     @Autowired
     private MovieRepository movieRepository;
 
-    public Movie add(Movie movie) {
-        return movieRepository.save(movie);
+    public Movie add(final String title, final String premiereTime) {
+        LocalDateTime premiereTimeParsed = LocalDateTime.parse(premiereTime, DateTimeFormatter.BASIC_ISO_DATE);
+        return movieRepository.save(new Movie(title, premiereTimeParsed));
     }
 
     public void remove(Long id) {
@@ -24,8 +29,15 @@ public class MovieService {
         return movieRepository.findAllByTitle(title);
     }
 
-    public Movie update(Movie movie) {
-        movieRepository.deleteById(movie.getId());
-        return movieRepository.save(movie);
+    public Movie update(final Long id, final String newTitle, final String newPremiereDate) throws NotFoundException {
+        Optional<Movie> movie = movieRepository.findById(id);
+        if (!movie.isPresent()) {
+            throw new NotFoundException("Brak filmu o podanym id");
+        } else {
+            movieRepository.deleteById(id);
+            movie.get().setTitle(newTitle);
+            movie.get().setPremiereDate(LocalDateTime.parse(newPremiereDate, DateTimeFormatter.BASIC_ISO_DATE));
+            return movieRepository.save(movie.get());
+        }
     }
 }
