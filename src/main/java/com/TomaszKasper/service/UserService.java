5bc1a84f2b3055630b022bf071d5e6c2388e749b
Tomasz Kasper <tomasz.kasperowicz@icloud.com>
2019-08-05 04:55:04 +0200

    followMovie UserService

--- src/main/java/com/TomaszKasper/service/UserService.java
+++ src/main/java/com/TomaszKasper/service/UserService.java
@@ -2,6 +2,7 @@ package com.TomaszKasper.service;
 
 import com.TomaszKasper.model.Movie;
 import com.TomaszKasper.model.User;
+import com.TomaszKasper.repository.MovieRepository;
 import com.TomaszKasper.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,6 +15,9 @@ public class UserService {
 
     @Autowired
     private UserRepository userRepository;
+
+    @Autowired
+    private MovieRepository movieRepository;
 
     public User create(final String username, final String password) throws Exception {
         if (userRepository.findByUsername(username).isPresent()) {
@@ -31,4 +35,21 @@ public class UserService {
             throw new Exception("Podany użytkownik nie istnieje");
         }
     }
+
+    public void addMovie(final String username, final String password, final Long movieId) throws Exception {
+        Optional<User> user = userRepository.findByUsername(username);
+        Optional<Movie> movie = movieRepository.findById(movieId);
+        if (!user.isPresent()) {
+            throw new Exception("Podany użytkownik nie istnieje");
+        }
+        if (!movie.isPresent()) {
+            throw new Exception("Film o podanym ID nie istnieje");
+        }
+
+        if (!user.get().getMovies().contains(movie)) {
+            user.get().getMovies().add(movie.get());
+            userRepository.save(user.get());
+        }
+    }
+
 }
