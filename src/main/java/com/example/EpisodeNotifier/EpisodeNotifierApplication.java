510d8ca7aeb32aae0d87d8e642a8cbb53ee8c809
Tomasz Kasper <tomasz.kasperowicz@icloud.com>
2019-08-21 21:32:53 +0200

    init

---
+++ src/main/java/com/example/EpisodeNotifier/EpisodeNotifierApplication.java
@@ -0,0 +1,13 @@
+package com.example.EpisodeNotifier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpisodeNotifierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EpisodeNotifierApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/EpisodeNotifier/EpisodeNotifierApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.EpisodeNotifier;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EpisodeNotifierApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
